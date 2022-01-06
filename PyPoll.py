d0637e444226eed31cdf6748046849c7f26f7d11
unknown <eduardoevperez@gmail.com>
2022-01-05 21:24:08 -0700

    Open election_results.csv in PyPoll using with open

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,10 @@
 # 3. The percentage of votes each candidate won 
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
+
+# Open election_result.csv
+file_to_load = 'Resources\election_results.csv'
+with open(file_to_load) as election_data:
+
+    # Perform Analysis
+    print(election_data)
