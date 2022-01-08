eed0f4e3f10bfc878af4a334afa9b73ba16a88c9
unknown <eduardoevperez@gmail.com>
2022-01-07 19:22:24 -0700

    Update PyPoll_Challenge.py with reading file info for county and add for loop for writing info into file

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -28,7 +28,8 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
+largest_county = ""
+largest_county_voter_turnout = 0
 
 
 # Read the csv and convert it into a list of dictionaries
@@ -48,7 +49,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -65,16 +66,17 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_name not in county_list:
 
             # 4b: Add the existing county to the list of counties.
-
+            county_list.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
+            county_votes[county_name] = 0
 
 
         # 5: Add a vote to that county's vote count.
-
+        county_votes[county_name] += 1
 
 
 # Save the results to our text file.
@@ -92,17 +94,17 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
-
+    for county_name in county_votes:
         # 6b: Retrieve the county vote count.
-
+        votes=county_votes.get(county_name)
         # 6c: Calculate the percentage of votes for the county.
+        vote_percentage = float(votes) / float(total_votes) * 100
 
+        # 6d: Print the county results to the terminal.
 
-         # 6d: Print the county results to the terminal.
+        # 6e: Save the county votes to a text file.
 
-         # 6e: Save the county votes to a text file.
-
-         # 6f: Write an if statement to determine the winning county and get its vote count.
+        # 6f: Write an if statement to determine the winning county and get its vote count.
 
 
     # 7: Print the county with the largest turnout to the terminal.
